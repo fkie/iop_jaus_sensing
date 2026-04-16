3234baa4751d9fb82de7a4b9ca7dbb176c58bf44
Alexander Tiderko <tiderko@tiderko2.cms.fkie.fraunhofer.de>
2026-04-16 10:11:11 +0200

    fix build for rolling

--- fkie_iop_range_sensor/include/urn_jaus_jss_environmentSensing_RangeSensor/RangeSensor_ReceiveFSM.h
+++ fkie_iop_range_sensor/include/urn_jaus_jss_environmentSensing_RangeSensor/RangeSensor_ReceiveFSM.h
@@ -44,8 +44,8 @@ along with this program; or you can read the full license at
 #include <rclcpp/rclcpp.hpp>
 #include <fkie_iop_component/iop_component.hpp>
 #include <sensor_msgs/msg/laser_scan.hpp>
-#include <tf2_ros/buffer.h>
-#include <tf2_ros/transform_listener.h>
+#include <tf2_ros/buffer.hpp>
+#include <tf2_ros/transform_listener.hpp>
 
 
 namespace urn_jaus_jss_environmentSensing_RangeSensor
